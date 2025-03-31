fab1892c824f158429f6f3b5c7de024ed1a09063
Jakub Owczarzak <kuba.owczarzak@wp.pl>
2025-03-31 21:49:55 +0200

    created class BankUser

---
+++ Models/BankUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystemProject.Models
+{
+    enum Roles
+    {
+        Admnin,
+        User
+    }
+    internal class BankUser
+    {
+        public string Username { get; set; }
+        private string Password { get; set; }
+        public Roles role { get; set; }
+
+        public BankUser(string username, string password, Roles role)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.role = role;
+        }
+        
+    }
+}
