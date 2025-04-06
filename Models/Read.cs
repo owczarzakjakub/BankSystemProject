3e3d6616e6130167f2a43c4318d54c82805484a2
piotr <piotr@DESKTOP-4I5BQL7>
2025-04-06 18:40:32 +0200

    klasa do haszowania i do operacjii na plku tekstowym(bazie danych)

---
+++ Models/Read.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Read()
+	{
+
+    public string ReadData(string data)
+    {
+        // Simulate reading data from a source
+        return $"Reading data: {data}";
+    }
+    public void DisplayData(string data)
+    {
+        Console.WriteLine(data);
+    }
+}
