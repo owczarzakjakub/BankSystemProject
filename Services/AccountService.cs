49b695b8b4b62734cca2c85ae2edae13a45a0a58
Jakub Owczarzak <kuba.owczarzak@wp.pl>
2025-04-16 17:04:55 +0200

    dodano fodler services (logika biznesowa programu), w nim dodano klase UserServices(obsluga konta uzytkownika), AccountServices(opercaje na koncie bankowym)

---
+++ Services/AccountService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BankSystemProject.Enums;
+using BankSystemProject.Models;
+
+
+using namespace BankSystemProject.Services;
+{
+	public class AccountService()
+	{
+		
+	}
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BankSystemProject.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using BankSystemProject.Models;
+
+namespace BankSystemProject.Services
+{
+    public class UserService
+    {
+        private readonly List<BankUser> _users;
+
+        public UserService()
+
+        public bool RegisterUser(string username, string password)
+        {
+            if (_users.Any(u => u.Username == username))
+            {
+                return false; 
+            }
+            var newUser = new BankUser
+            {
+                Username = username,
+                Password = HashPassword(password),
+                Roles = new List<Role> { Role.User }
+
+            };
+            _users.Add(newUser);
+            return true;
+        }
+
+        public BankUser? Login(string username, string password)
+        {
+            return _users.FirstOrDefault(u => u.Username == username && u.Password == HashPassword(password));
+        }
+
+        public List<BankUser> GetAllUsers()
+        {
+            return _users;
+        }
+
+    }
+}
